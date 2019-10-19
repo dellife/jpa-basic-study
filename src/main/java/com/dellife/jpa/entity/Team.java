f2378622f15d4c3d5707d7764f2bfcc543ac5a96
dellife <seheekwon21@gmail.com>
2019-10-19 23:56:17 +0900

    jpql collection fetch join

--- src/main/java/com/dellife/jpa/entity/Team.java
+++ src/main/java/com/dellife/jpa/entity/Team.java
@@ -3,6 +3,7 @@ package com.dellife.jpa.entity;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -20,6 +21,7 @@ public class Team {
     private Long id;
     private String name;
 
+//    @BatchSize(size = 100)  //collection fetch join 안되는 문제 해결 방법 중 하나.
     @OneToMany(mappedBy = "team")
     private List<Member> members;
 }
--- src/main/java/com/dellife/jpa/jpql/fetchJoinMain.java
+++ src/main/java/com/dellife/jpa/jpql/fetchJoinMain.java
@@ -59,14 +59,41 @@ public class fetchJoinMain {
 
             // 위 쿼리 수행 후 아래 team의 이름을 roop 돌면서 찍으면 lazy loading 이기 때문에 loop 도는 만큼 team 조회 쿼리가 나감.
             // 단, 같은 team의 경우 프록시로 가져왔기 때문에 아래 수행하면 3번 돌지만, team 조회쿼리 2번 나갈 것임. -> 100번이라면 100번 db 조회하는 문제 생김.
-            result.forEach( m -> System.out.println(m.getTeam().getName()) );
+            result.forEach(m -> System.out.println(m.getTeam().getName()));
 
 
             // jpql 에서의 fetch join 사용 방법
             // 한번의 member 와 team 모두 한방 쿼리로 조회한다! 위의 n+1 문제 해결.
             String fetchQuery = "select m from Member m join fetch m.team";
             List<Member> result2 = em.createQuery(fetchQuery, Member.class).getResultList();
-            result2.forEach( m -> System.out.println(m.getTeam().getName()) );
+            result2.forEach(m -> System.out.println(m.getTeam().getName()));
+
+
+            // collection 을 fetch join 하는 방법.
+            // 이렇게 할 경우 one to many 이기 때문에 중복으로 데이터를 가져오게 된다.
+            String collectionFetchQuery = "select t from Team t join fetch t.members";
+            List<Team> result3 = em.createQuery(collectionFetchQuery, Team.class).getResultList();
+
+            for (Team team1 : result3) {
+                System.out.println("team : " + team1.getName() + ", member size: " + team1.getMembers().size());
+            }
+
+            // jpql 의 DISTINCT
+            // 1. SQL의 DISTINCT 추가, 2. 애플리케이션의 엔티티 중복 제거
+            String distinctQuery = "select distinct t from Team t join fetch t.members";
+            List<Team> result4 = em.createQuery(distinctQuery, Team.class).getResultList();
+
+            for (Team team1 : result4) {
+                System.out.println("team : " + team1.getName() + ", member size: " + team1.getMembers().size());
+            }
+
+            /*
+            fetch join 의 특징과 한계
+            1. fetch join 대상에는 별칭을 줄 수 없다. -> 별칭으로 where 문에 조건을 줄 수 없다.
+            2. 둘 이상컬렉션은 페치조인할 수 없다.
+            3. 컬렉션을 페치조인하면 페이징API를 사용할 수 없다. (일대일, 다대일은 가능, 하이버네이트는 메모리에서 구현되어 위험)
+
+             */
 
 
             tx.commit();
